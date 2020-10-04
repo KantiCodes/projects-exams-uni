a72bcaf98d031382765dfd1f904796c8ba7070a5
eviluser <jendrekpb@gmail.com>
2020-10-04 16:56:48 +0200

    addded work from the F# seminar

---
+++ BA_degree/2nd Semester/MapReduceFSharp/MapReduceFSharp/Program.fs
@@ -0,0 +1,12 @@
+﻿//Map Reduce
+//Counting evens
+
+let numbers =[0..10000000]
+
+let res =
+    numbers
+    |> List.map(fun x -> if x % 7 = 0 then 1 else 0)
+    |> List.fold (+) 0
+    
+printfn""
+printfn"Piping: |> map evens |> sum ones= %A" res 
---
+++ BA_degree/2nd Semester/ParallelMapReduce/ParallelMapReduce/Program.fs
@@ -0,0 +1,26 @@
+﻿//    let numbers = [0..50000000]
+//    let oneBigArray = List.toArray numbers
+    let rec slowdown n = if n=0 then 0 else slowdown(n-1)
+    let oneBigArray = [| 0 .. 50000000 |]
+
+    //Counting evens sequential:
+    printfn "Starting sequential counting evens: %A" ""
+    let res =
+       oneBigArray 
+       |> Array.map (fun x ->  ignore(slowdown(100))
+                               if x % 2 = 0 then 1 else 0)
+       |> Array.fold (+) 0
+          
+    printfn "Done sequential counting evens: %A" (res)
+
+    //Counting evens parallel:
+    printfn "Starting parallel counting evens: %A" ""
+    let res1 =
+       oneBigArray 
+       |> Array.Parallel.map (fun x ->  ignore(slowdown(100))
+                                        if x % 2 = 0 then 1 else 0)
+       |> Array.fold (+) 0
+          
+    printfn "Done parallel counting evens: %A" (res1)
+
+  
